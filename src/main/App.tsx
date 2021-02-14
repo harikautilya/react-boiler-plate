93b47386e331e04c158ede246f28ef0a3400694e
Kautilya Hari <kautilya@janitri.in>
2021-02-14 23:20:25 +0530

    Adding router to biolerplate

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,40 +1,31 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import 'main/App.css';
+
 import { Neomorphic } from 'components/neomorphic/neomorphic';
 import { Section } from 'layouts/section/section';
 import { Row } from 'layouts/row/row';
 import { Column } from 'layouts/column/column';
+import { Home } from 'pages/home/home';
 
 function App() {
-  const bodyContent = 'Remember sky is not the limit';
-  const projectContent = 'Start the project';
   return (
-    <Section style={{ background: 'var(--primary-color)' }} orientation="vertical" fullscreen centerVertical centerHorizontal>
-      <Row style={{ flexWrap: 'nowrap' }}>
-        <Neomorphic>
-          <Column style={{ padding: '4rem' }} center>
-            <p style={{ color: 'white', fontSize: '60px' }}>K</p>
-            <p style={{ color: 'white', fontSize: '12px' }}>{ projectContent }</p>
-            <p style={{ color: 'white', fontSize: '12px' }}>{ bodyContent }</p>
-          </Column>
-        </Neomorphic>
-        <Neomorphic style={{ margin: '0 3rem' }}>
-          <Column style={{ padding: '4rem' }} center>
-            <p style={{ color: 'white', fontSize: '60px' }}>K</p>
-            <p style={{ color: 'white', fontSize: '12px' }}>{ projectContent }</p>
-            <p style={{ color: 'white', fontSize: '12px' }}>{ bodyContent }</p>
-          </Column>
-        </Neomorphic>
-        <Neomorphic style={{ margin: '0 3rem' }} active>
-          <Column style={{ padding: '4rem' }} center>
-            <p style={{ color: 'white', fontSize: '60px' }}>K</p>
-            <p style={{ color: 'white', fontSize: '12px' }}>{ projectContent }</p>
-            <p style={{ color: 'white', fontSize: '12px' }}>{ bodyContent }</p>
-          </Column>
-        </Neomorphic>
-      </Row>
-    </Section>
+    <Router>
+      <div className="main_body">
+        <div className="page">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
